2c00d0af6c2788e924ae44ca3b2f7f5b982ddfba
Amr Mohamed <amr_mohamed@live.com>
2015-10-31 01:55:23 +0200

    Uploaded the project

---
+++ hello_app/app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def hello
+    render text: "Append your name here: 1)Amr Mohamed"
+  end
+  protect_from_forgery with: :exception
+end
