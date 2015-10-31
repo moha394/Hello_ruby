5ce68712c5ade8c7f9c9787127237f2088b24408
thabetx <thabetx@gmail.com>
2015-10-31 22:53:08 +0200

    adds my name (thabet) and some line breaks, the app works fine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def hello
-    render text: "Add your name here: 1)Amr Mohamed, 2) Amr Alaa"
+    render text: "Add your name here:<br>
+                  1) Amr Mohamed<br>
+                  2) Amr Alaa <br>
+                  3) Thabet<br>"
   end
   protect_from_forgery with: :exception
 end
