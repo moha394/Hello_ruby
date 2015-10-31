c9009d8bcaa18928a6e814afb2299855a99cdd6a
Amr Alaa <n1amr1@gmail.com>
2015-10-31 16:12:42 +0200

    Amr Alaa signs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def hello
-    render text: "Append your name here: 1)Amr Mohamed"
+    render text: "Append your name here: 1)Amr Mohamed, 2) Amr Alaa"
   end
   protect_from_forgery with: :exception
 end
